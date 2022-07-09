e9e4c5cdd99e290b59acd74c090cfee0d3a3b7d0
ahmedwelhakim <ahmed.w.elhakim@gmail.com>
2022-07-09 23:26:20 +0200

    adding mobile phone compatibility

--- js/key.js
+++ js/key.js
@@ -16,3 +16,23 @@ document.onkeydown = function userInput() {
             break;
     }
 }
+var myCanvas = document.getElementById("myCanvas");
+
+myCanvas.addEventListener("mouseup", tapOrClick, false);
+myCanvas.addEventListener("touchend", tapOrClick, false);
+
+function tapOrClick(event) {
+   //handle tap or click.
+
+    event.preventDefault();
+    if (state.current == state.ready) {
+        state.current = state.play;
+        birdFlap();
+    } else if (state.current == state.play) {
+        birdFlap();
+    } else {
+        state.current = state.ready;
+        reset();
+    }
+    return false;
+}
